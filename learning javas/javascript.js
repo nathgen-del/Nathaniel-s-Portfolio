3cb9334220151c1d5a321663dabd066d4a8f7fad
nathgen-del <nathanielgenato@gmail.com>
2026-05-26 17:04:19 +0800

    First website upload

---
+++ learning javas/javascript.js
@@ -0,0 +1,7 @@
+function loginintrution(username, ipAddress) {
+    console.log("Warnning: Unauthorized access is attemted by: "+ username + " From " + ipAddress);
+}
+
+loginintrution('Guessnaths', '192.190.1.1');
+
+ 
